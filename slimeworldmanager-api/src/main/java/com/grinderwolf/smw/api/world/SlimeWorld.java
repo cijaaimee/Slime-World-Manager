4f4073cf1deefc4fc24d6fdc43acea8f7b3c3943
Grinderwolf <jaimecostasinsua@gmail.com>
2019-07-31 12:40:18 +0200

    Update javadocs.

--- slimeworldmanager-api/src/main/java/com/grinderwolf/smw/api/world/SlimeWorld.java
+++ slimeworldmanager-api/src/main/java/com/grinderwolf/smw/api/world/SlimeWorld.java
@@ -6,14 +6,56 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
+/**
+ * In-memory representation of a SRF world.
+ */
 public interface SlimeWorld {
 
+    /**
+     * Returns the name of the world.
+     *
+     * @return The name of the world.
+     */
     public String getName();
+
+    /**
+     * Returns the {@link SlimeLoader} used
+     * to load and store the world.
+     *
+     * @return The {@link SlimeLoader} used to load and store the world.
+     */
     public SlimeLoader getLoader();
+
+    /**
+     * Returns the chunk that belongs to the coordinates specified.
+     *
+     * @param x X coordinate.
+     * @param z Z coordinate.
+     *
+     * @return The {@link SlimeChunk} that belongs to those coordinates.
+     */
     public SlimeChunk getChunk(int x, int z);
+
+    /**
+     * Returns the extra data of the world. Inside this {@link CompoundTag}
+     * can be stored any information, to then be retrieved later, as it's
+     * saved alongside the world data.
+     *
+     * @return A {@link CompoundTag} containing the extra data of the world.
+     */
     public CompoundTag getExtraData();
+
+    /**
+     * Returns the properties of the world. These properties are automatically
+     * kept up-to-date when the world is loaded and its properties are updated.
+     *
+     * @return A {@link SlimeProperties} object with all the current properties of the world.
+     */
     public SlimeProperties getProperties();
 
+    /**
+     * All the currently-available properties of the world.
+     */
     @Getter
     @Builder(toBuilder = true)
     public class SlimeProperties {
