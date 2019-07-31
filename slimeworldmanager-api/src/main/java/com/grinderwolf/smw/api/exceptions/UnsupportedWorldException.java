78eff52db3fa47b9bd6edc5eeb0c115ab84b45a7
Grinderwolf <jaimecostasinsua@gmail.com>
2019-07-31 14:43:50 +0200

    Fixed typo in javadocs.

--- slimeworldmanager-api/src/main/java/com/grinderwolf/smw/api/exceptions/UnsupportedWorldException.java
+++ slimeworldmanager-api/src/main/java/com/grinderwolf/smw/api/exceptions/UnsupportedWorldException.java
@@ -3,7 +3,7 @@ package com.grinderwolf.smw.api.exceptions;
 import lombok.Getter;
 
 /**
- * Exception thrown when a 1.13 or newer is loaded
+ * Exception thrown when a 1.13 or newer world is loaded
  * on a 1.12.2 or older server and vice versa.
  */
 public class UnsupportedWorldException extends SlimeException {
