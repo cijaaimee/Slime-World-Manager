0853108f7f5b36fbf15eec79c12d5a9e95717066
Grinderwolf <jaimecostasinsua@gmail.com>
2019-09-27 17:38:27 +0200

    Don't update world properties.

--- slimeworldmanager-nms-v1_13_R2/src/main/java/com/grinderwolf/swm/nms/v1_13_R2/CustomWorldData.java
+++ slimeworldmanager-nms-v1_13_R2/src/main/java/com/grinderwolf/swm/nms/v1_13_R2/CustomWorldData.java
@@ -1,11 +1,8 @@
 package com.grinderwolf.swm.nms.v1_13_R2;
 
 import com.flowpowered.nbt.CompoundTag;
-import com.grinderwolf.swm.api.world.SlimeWorld;
 import com.grinderwolf.swm.nms.CraftSlimeWorld;
 import lombok.Getter;
-import net.minecraft.server.v1_13_R2.BlockPosition;
-import net.minecraft.server.v1_13_R2.EnumDifficulty;
 import net.minecraft.server.v1_13_R2.EnumGamemode;
 import net.minecraft.server.v1_13_R2.NBTTagCompound;
 import net.minecraft.server.v1_13_R2.WorldData;
@@ -31,30 +28,5 @@ public class CustomWorldData extends WorldData {
     public String getName() {
         return world.getName();
     }
-
-    @Override
-    public void setSpawn(BlockPosition position) {
-        super.setSpawn(position);
-
-        // Keep properties updated
-        SlimeWorld.SlimeProperties oldProps = world.getProperties();
-
-        if (oldProps.getSpawnX() != position.getX() || oldProps.getSpawnY() != position.getY() || oldProps.getSpawnZ() != position.getZ()) {
-            SlimeWorld.SlimeProperties newProps = oldProps.toBuilder().spawnX(position.getX()).spawnY(position.getY()).spawnZ(position.getZ()).build();
-            world.setProperties(newProps);
-        }
-    }
-
-    @Override
-    public void setDifficulty(EnumDifficulty difficulty) {
-        super.setDifficulty(difficulty);
-
-        // Keep properties updated
-        SlimeWorld.SlimeProperties oldProps = world.getProperties();
-
-        if (oldProps.getDifficulty() != difficulty.a()) {
-            SlimeWorld.SlimeProperties newProps = oldProps.toBuilder().difficulty(difficulty.a()).build();
-            world.setProperties(newProps);
-        }
-    }
+    
 }
