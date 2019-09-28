39e34d92a761195e97bf7c39785550e6f8060c5c
Grinderwolf <jaimecostasinsua@gmail.com>
2019-09-28 20:02:11 +0200

    Update release.sh

--- .travis/release.sh
+++ .travis/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Ensuring that pom  matches $TRAVIS_TAG"
-./mvnw org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
+mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
 
 echo "Uploading to oss repo and GitHub"
-./mvnw deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
