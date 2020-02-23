da9f162cba0b051f061d74f08986ea780188c371
Gerolmed <werwolf420@yahoo.de>
2020-02-23 13:32:23 +0100

    Removed travis ci

--- .travis/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "Ensuring that pom  matches $TRAVIS_TAG"
-mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
-
-echo "Uploading to oss repo and GitHub"
-mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
