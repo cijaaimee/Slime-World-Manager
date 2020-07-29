e31a37237fde8ec1fb728989df87bf519f441f3f
Julian Mills <julian.exodius@gmail.com>
2020-07-29 02:42:37 +0200

    feat: apply gamerules from world extra data

--- slimeworldmanager-nms-v1_16_R1/src/main/java/com/grinderwolf/swm/nms/v1_16_R1/CustomWorldData.java
+++ slimeworldmanager-nms-v1_16_R1/src/main/java/com/grinderwolf/swm/nms/v1_16_R1/CustomWorldData.java
@@ -3,11 +3,19 @@ package com.grinderwolf.swm.nms.v1_16_R1;
 import com.flowpowered.nbt.CompoundTag;
 import com.grinderwolf.swm.api.world.properties.SlimeProperties;
 import com.grinderwolf.swm.nms.CraftSlimeWorld;
+import com.mojang.serialization.DynamicLike;
 import com.mojang.serialization.Lifecycle;
 import lombok.Getter;
 import net.minecraft.server.v1_16_R1.*;
+import net.minecraft.server.v1_16_R1.GameRules.GameRuleInt;
+import net.minecraft.server.v1_16_R1.GameRules.GameRuleKey;
+import net.minecraft.server.v1_16_R1.GameRules.GameRuleValue;
+import org.bukkit.Bukkit;
 import org.bukkit.WorldType;
+import org.bukkit.craftbukkit.v1_16_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_16_R1.CraftWorld;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Getter
@@ -38,7 +46,21 @@ public class CustomWorldData extends WorldDataServer {
         // Game rules
         CompoundTag extraData = world.getExtraData();
         Optional<CompoundTag> gameRules = extraData.getAsCompoundTag("gamerules");
-        gameRules.ifPresent(compoundTag -> this.a((NBTTagCompound) Converter.convertTag(compoundTag)));
+
+        gameRules.ifPresent(compoundTag -> {
+            NBTTagCompound compound = (NBTTagCompound) Converter.convertTag(compoundTag);
+            Map<String, GameRuleKey<?>> gameRuleKeys = CraftWorld.getGameRulesNMS();
+            MinecraftServer mcServer = MinecraftServer.getServer();
+
+            compound.getKeys().forEach(gameRule -> {
+                if(gameRuleKeys.containsKey(gameRule)) {
+                    GameRuleValue<?> gameRuleValue = this.p().get(gameRuleKeys.get(gameRule));
+                    String theValue = compound.getString(gameRule);
+                    gameRuleValue.setValue(theValue);
+                    gameRuleValue.onChange(mcServer);
+                }
+            });
+        });
     }
 
     public String getName() {
