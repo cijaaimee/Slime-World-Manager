03a49e7c1b8ce16204ee720b2091496047c17b99
ramdon_person <48343703+fierceeo@users.noreply.github.com>
2021-07-03 23:15:20 -0700

    Update NMSSlimeChunk.java

--- slimeworldmanager-nms-v1_17_R1/src/main/java/com/grinderwolf/swm/v1_17_R1/NMSSlimeChunk.java
+++ slimeworldmanager-nms-v1_17_R1/src/main/java/com/grinderwolf/swm/v1_17_R1/NMSSlimeChunk.java
@@ -122,7 +122,7 @@ public class NMSSlimeChunk implements SlimeChunk {
     public List<CompoundTag> getEntities() {
         List<CompoundTag> entities = new ArrayList<>();
 
-        PersistentEntitySectionManager<Entity> entitySectionManager = entitySectionManager = chunk.i.G;
+        PersistentEntitySectionManager<Entity> entitySectionManager = entitySectionManager = chunk.level.G;
 
         Iterator<Entity> entityIterable = entitySectionManager.d().a().iterator();
 
