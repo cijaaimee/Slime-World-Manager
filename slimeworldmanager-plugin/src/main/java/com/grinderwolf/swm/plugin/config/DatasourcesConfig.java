e61a10d592ed371b4c8466f5d49ba58f5947b581
dementisimus <54750441+dementisimus@users.noreply.github.com>
2021-09-30 12:40:59 +0200

    Permit API-Users to configure the DatasourcesConfig on runtime (#66)

--- slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/config/DatasourcesConfig.java
+++ slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/config/DatasourcesConfig.java
@@ -1,7 +1,7 @@
 package com.grinderwolf.swm.plugin.config;
 
-import io.lettuce.core.RedisURI;
 import lombok.Getter;
+import lombok.Setter;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
@@ -9,62 +9,110 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 @ConfigSerializable
 public class DatasourcesConfig {
 
-    @Setting("file") private FileConfig fileConfig = new FileConfig();
-    @Setting("mysql") private MysqlConfig mysqlConfig = new MysqlConfig();
-    @Setting("mongodb") private MongoDBConfig mongoDbConfig = new MongoDBConfig();
-    @Setting("redis") private RedisConfig redisConfig = new RedisConfig();
+    @Setting("file")
+    @Setter
+    private FileConfig fileConfig = new FileConfig();
+    @Setting("mysql")
+    @Setter
+    private MysqlConfig mysqlConfig = new MysqlConfig();
+    @Setting("mongodb")
+    @Setter
+    private MongoDBConfig mongoDbConfig = new MongoDBConfig();
+    @Setting("redis")
+    @Setter
+    private RedisConfig redisConfig = new RedisConfig();
 
     @Getter
     @ConfigSerializable
     public static class MysqlConfig {
 
-        @Setting("enabled") private boolean enabled = false;
+        @Setting("enabled")
+        @Setter
+        private boolean enabled = false;
 
-        @Setting("host") private String host = "127.0.0.1";
-        @Setting("port") private int port = 3306;
+        @Setting("host")
+        @Setter
+        private String host = "127.0.0.1";
+        @Setting("port")
+        @Setter
+        private int port = 3306;
 
-        @Setting("username") private String username = "slimeworldmanager";
-        @Setting("password") private String password = "";
+        @Setting("username")
+        @Setter
+        private String username = "slimeworldmanager";
+        @Setting("password")
+        @Setter
+        private String password = "";
 
-        @Setting("database") private String database = "slimeworldmanager";
+        @Setting("database")
+        @Setter
+        private String database = "slimeworldmanager";
 
-        @Setting("usessl") private boolean usessl = false;
+        @Setting("usessl")
+        @Setter
+        private boolean usessl = false;
 
-        @Setting("sqlUrl") private String sqlUrl = "jdbc:mysql://{host}:{port}/{database}?autoReconnect=true&allowMultiQueries=true&useSSL={usessl}";
+        @Setting("sqlUrl")
+        @Setter
+        private String sqlUrl = "jdbc:mysql://{host}:{port}/{database}?autoReconnect=true&allowMultiQueries=true&useSSL={usessl}";
     }
 
     @Getter
     @ConfigSerializable
     public static class MongoDBConfig {
 
-        @Setting("enabled") private boolean enabled = false;
+        @Setting("enabled")
+        @Setter
+        private boolean enabled = false;
 
-        @Setting("host") private String host = "127.0.0.1";
-        @Setting("port") private int port = 27017;
+        @Setting("host")
+        @Setter
+        private String host = "127.0.0.1";
+        @Setting("port")
+        @Setter
+        private int port = 27017;
 
-        @Setting("auth") private String authSource = "admin";
-        @Setting("username") private String username = "slimeworldmanager";
-        @Setting("password") private String password = "";
+        @Setting("auth")
+        @Setter
+        private String authSource = "admin";
+        @Setting("username")
+        @Setter
+        private String username = "slimeworldmanager";
+        @Setting("password")
+        @Setter
+        private String password = "";
 
-        @Setting("database") private String database = "slimeworldmanager";
-        @Setting("collection") private String collection = "worlds";
+        @Setting("database")
+        @Setter
+        private String database = "slimeworldmanager";
+        @Setting("collection")
+        @Setter
+        private String collection = "worlds";
 
-        @Setting("uri") private String uri = "";
+        @Setting("uri")
+        @Setter
+        private String uri = "";
     }
 
     @Getter
     @ConfigSerializable
     public static class FileConfig {
 
-        @Setting("path") private String path = "slime_worlds";
+        @Setting("path")
+        @Setter
+        private String path = "slime_worlds";
 
     }
 
-  @Getter
-  @ConfigSerializable
-  public static class RedisConfig {
+    @Getter
+    @ConfigSerializable
+    public static class RedisConfig {
 
-    @Setting("enabled") private boolean enabled = false;
-    @Setting("uri") private String uri = "redis://127.0.0.1/";
-  }
+        @Setting("enabled")
+        @Setter
+        private boolean enabled = false;
+        @Setting("uri")
+        @Setter
+        private String uri = "redis://127.0.0.1/";
+    }
 }
